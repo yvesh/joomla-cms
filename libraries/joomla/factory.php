f997f6ec9cacffe3cd60f558301410340adb447d
Yves Hoppe <yves.hoppe@community.joomla.org>
2017-04-12 22:36:13 +0200

    [4.0] Uppercase CMS where possible

--- libraries/joomla/factory.php
+++ libraries/joomla/factory.php
@@ -120,7 +120,7 @@ abstract class JFactory
 	 * @param   string     $prefix     Application prefix
 	 * @param   Container  $container  An optional dependency injection container to inject into the application.
 	 *
-	 * @return  \Joomla\CMS\Application\CmsApplication object
+	 * @return  \Joomla\CMS\Application\CMSApplication object
 	 *
 	 * @see     JApplication
 	 * @since   11.1
